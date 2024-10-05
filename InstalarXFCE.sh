258e11c8254312ee5b4380f7f0242d20bc8fad4f
Efrainett <efrainchnt@gmail.com>
2024-10-05 17:04:20 -0300

    Update InstalarXFCE.sh

--- InstalarXFCE.sh
+++ InstalarXFCE.sh
@@ -3,7 +3,7 @@ apt update
 apt-get install firmware-linux-nonfree
 apt-get install firmware-linux-nonfree
 apt update
-apt-get install xfdesktop4 xfwm4 xfce4-panel xfce4-settings xfce4-session xfce4-terminal thunar xfce4-whiskermenu-plugin xfce4-power-manager network-manager-gnome xinput xserver-xorg-input-synaptics lightdm-settings
+apt-get install xfdesktop4 xfwm4 xfce4-panel xfce4-settings xfce4-session xfce4-terminal thunar xfce4-whiskermenu-plugin xfce4-power-manager network-manager-gnome xinput libinput-tools xserver-xorg-input-synaptics lightdm-settings
 apt-get install pipewire pulseaudio-utils alsa-utils xfce4-pulseaudio-plugin
 apt-get install sudo
 apt-get install firefox-esr
